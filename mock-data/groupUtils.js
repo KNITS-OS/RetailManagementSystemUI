7d39cbeed4e3e856df34edf83b98386745f7bc6b
nikitaPodkopaev <79059529+nikitaPodkopaev@users.noreply.github.com>
2022-01-13 02:45:11 -0500

    RMS 3-30
    Have completed UI and Redux parts for the tickets: RMS-3, RMS-4, RMS-7, RMS-30

---
+++ mock-data/groupUtils.js
@@ -0,0 +1,14 @@
+import { employeesData as employees } from "./employees.js";
+
+export const employeesFromIds = (employeeIds) => {
+  let groupMembers = employees.filter((employee) => {
+    for (let empId of employeeIds) {
+      if (employee.id === empId) {
+        return true;
+      }
+    }
+
+    return false;
+  });
+  return groupMembers;
+};
